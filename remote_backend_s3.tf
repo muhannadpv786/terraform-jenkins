b3388671ea05e89edf574991d0aa58f3a29ce1b5
muhannadpv786 <p.vmuhannad786@gmail.com>
2025-02-05 11:08:09 +0530

    Update remote_backend_s3.tf

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-jenkins-remote-state-bucket-123456"
+    bucket = "muhannad-proj-1-jenkins-remote-state-bucket-123456"
     key    = "devops-project-1/jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
